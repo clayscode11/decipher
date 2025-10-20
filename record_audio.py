15647aca0ec9d8e6b3ecbbfa4e9228032d58a598
clayscode11 <bartlettclayton0@gmail.com>
2025-10-19 22:30:23 -0700

    added untracked folder for test audio files

--- record_audio.py
+++ record_audio.py
@@ -3,6 +3,9 @@ import keyboard
 import wave
 import time
 import winsound
+import os
+
+print(os.getcwd())
 
 audio_recordings = []
 
@@ -10,9 +13,10 @@ format = pyaudio.paInt16 # 16 bit
 channels = 1 # mono audio channel
 rate = 44100 # Sample rate
 chunk = 1024 # frames in buffer
-output_filename = "recordedFile.wav" # file name provsioning
+output_filename = 'recordedFile.wav' # file name provsioning
+file_path = f'audioFiles/{output_filename}'    
 
-audio = pyaudio.PyAudio() # init pyaudio
+audio = pyaudio.PyAudio() # init pyaudio  
 stream = audio.open(format=format, channels=channels, rate=rate, input=True, frames_per_buffer=chunk) # opening the new audio stream
 
 frames = []
@@ -20,7 +24,7 @@ print("Press SPACE to start recording")
 keyboard.wait("space")
 print("Recording... Press SPACE to end recording.")
 time.sleep(0.2) # gives buffer to account for physical input delays
-
+   
 while True:
     try:
         data = stream.read(chunk)
@@ -36,10 +40,9 @@ stream.stop_stream()
 stream.close()  
 audio.terminate()
 
-wave_file = wave.open(output_filename, "wb")
+wave_file = wave.open(file_path, "wb")
 wave_file.setnchannels(channels)
-wave_file.setsampwidth(audio.get_sample_size(format))  
-
+wave_file.setsampwidth(audio.get_sample_size(format))    
 wave_file.setframerate(rate)
 wave_file.writeframes(b''.join(frames))
 wave_file.close()
@@ -49,10 +52,9 @@ keyboard.wait('enter')
   
 if keyboard.is_pressed('enter'):
     try:
-        winsound.PlaySound('recordedFile.wav', winsound.SND_FILENAME)
+        winsound.PlaySound('audioFiles/recordedFile.wav', winsound.SND_FILENAME)
     except Exception as e:
         print("Error playing recorded file")  
 
                 
-            
             
