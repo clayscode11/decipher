2a75dff879aa49b0d503c678c6cf628d409eeef6
clayscode11 <bartlettclayton0@gmail.com>
2025-10-20 22:01:57 -0700

    modularized the recording into a function

---
+++ recorder.py
@@ -0,0 +1,38 @@
+import pyaudio
+import keyboard
+import wave
+import time
+
+def record():
+    audio_recordings = []
+
+    format = pyaudio.paInt16
+    channels = 1
+    rate = 44100
+    chunk = 1024
+    output_filename = 'recorded_file.wav'
+    file_path = f'audioFiles/{output_filename}'
+
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=format, channels=channels, rate=rate, input=True, frames_per_buffer=chunk)
+    duration = 5
+    start_time = time.time()
+
+    frames = []
+
+    while time.time() - start_time <= duration:
+        data = stream.read(chunk)
+        frames.append(data)
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    wave_file = wave.open(file_path, 'wb')
+    wave_file.setnchannels(channels)
+    wave_file.setsampwidth(audio.get_sample_size(format))
+    wave_file.setframerate(rate)
+    wave_file.writeframes(b''.join(frames))
+    wave_file.close()
+
+record()
